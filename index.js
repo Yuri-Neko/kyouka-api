d3b789c9e6040be3293e4fe243ef40993eaf2e54
Kyouka Hashiba <91820453+Yuri-Neko@users.noreply.github.com>
2023-04-09 12:07:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,56 @@
 const axios = require('axios');
 
-class OtakudesuAPI {
+class kyouka_api {
   constructor(apiKey) {
     this.apiKey = apiKey;
-    this.baseURL = 'https://web.api-kyouka.my.id/api/anime/otakudesu';
+    this.otakuDesuBaseURL = 'https://web.api-kyouka.my.id/api/anime/otakudesu';
+    this.igoDesuBaseURL = 'https://web.api-kyouka.my.id/api/nsfw/igodesu';
   }
 
-  async otakusearch(query) {
+  async otakusearch(query, result = 10) {
     try {
-      const response = await axios.get(`${this.baseURL}/detail?apikey=${this.apiKey}&query=${query}`);
+      const response = await axios.get(`${this.otakuDesuBaseURL}/search?apikey=${this.apiKey}&query=${query}&result=${result}`);
       return response.data;
     } catch (error) {
       throw new Error(`Failed to search anime: ${error.message}`);
     }
   }
-  async otakudetail(url) {
-    try {
-      const response = await axios.get(`${this.baseURL}/detail?apikey=${this.apiKey}&url=${url}`);
-      return response.data;
-    } catch (error) {
-      throw new Error(`Failed to search anime: ${error.message}`);
-    }
-  }
-  async otakudownload(url) {
-    try {
-      const episodeUrl = url.replace('https://otakudesu.lol/episode/', '');
-      const response = await axios.get(`${this.baseURL}/downloadv2?apikey=${this.apiKey}&id=${episodeUrl}`);
-      return response.data;
-    } catch (error) {
-      throw new Error(`Failed to get episode download URL: ${error.message}`);
-    }
-  }
-}
+
   async otakulatest() {
     try {
-      const response = await axios.get(`${this.baseURL}/latest?apikey=${this.apiKey}`);
+      const response = await axios.get(`${this.otakuDesuBaseURL}/latest?apikey=${this.apiKey}`);
       return response.data;
     } catch (error) {
       throw new Error(`Failed to get latest anime: ${error.message}`);
     }
   }
+
+  async otakudownload(id) {
+    try {
+      const response = await axios.get(`${this.otakuDesuBaseURL}/downloadv2?apikey=${this.apiKey}&id=${id}`);
+      return response.data;
+    } catch (error) {
+      throw new Error(`Failed to download anime: ${error.message}`);
+    }
+  }
+  
+ async otakudetail(url) {
+    try {
+      const response = await axios.get(`${this.otakuDesuBaseURL}/detail?apikey=${this.apiKey}&url=${url}`);
+      return response.data;
+    } catch (error) {
+      throw new Error(`Failed to get anime details: ${error.message}`);
+    }
+  }
+  
+  async igodesulatest() {
+    try {
+      const response = await axios.get(`${this.igoDesuBaseURL}/latest?apikey=${this.apiKey}`);
+      return response.data;
+    } catch (error) {
+      throw new Error(`Failed to get latest releases from IgoDesu: ${error.message}`);
+    }
+  }
 }
 
-
-
-module.exports = OtakudesuAPI;
+module.exports = kyouka_api;
